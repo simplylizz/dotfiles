243df5b74d927dc6aac667b51fc727112bfbbdf9
Anton V. Yanchenko <simplylizz@gmail.com>
2012-08-17 17:59:34 +0400

    more lxde configs

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# lxde things
+sudo apt-get install lxde openbox lxtasks gmrun
+
 # TODO: add backups
-cp -rf .vimrc .vim .zshrc .gitconfig ~/ && vim +BundleInstall +qall
+cp -rf .vimrc .vim .zshrc .gitconfig .config ~/
+vim +BundleInstall +qall
