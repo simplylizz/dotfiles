1488a40a9428a8303de3506127f5342b6f94339c
Anton Yanchenko <simplylizz@gmail.com>
2020-12-19 09:12:08 +0100

    Adjust git config

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 # TODO: add backups
 # TODO: make links, not copy
 git submodule update --init --recursive
-cp -rf .vimrc .vim .zshrc* .oh-my-zsh .gitconfig .config ~/
+cp -rf .vimrc .vim .zshrc* .oh-my-zsh .gitconfig .gitignore_global .config ~/
 vim +BundleInstall +qall
