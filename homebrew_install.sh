fc4692df74f29e814874dc249128fef124a640e6
Anton Yanchenko <simplylizz@gmail.com>
2020-12-19 09:13:45 +0100

    Add homebrew_install.sh helper

---
+++ homebrew_install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+# Hmm, it has even iterm2. Should I install it through it though?
+# More cli tools to check:
+# https://devzen.ru/episode-0312/
+homebrew install \
+    vim \
+    git \
+    rg \
+    fd \
+    exa \
+    fzf \
+    jump \
+    mdcat
+
