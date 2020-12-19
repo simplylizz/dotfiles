cb236621213675bd6f5c6ca9b8c028c6c493b160
Anton Yanchenko <simplylizz@gmail.com>
2020-12-19 22:27:58 +0100

    Use symlinks for most of configs
    And git clone instead of submodules.

--- install.sh
+++ install.sh
@@ -3,12 +3,31 @@
 set -eo pipefail
 
 # TODO: add backups
-# TODO: make links, not copy
+
 git submodule update --init --recursive
-cp -rf .vimrc .vim .zshrc* .oh-my-zsh .gitconfig .gitignore_global .config ~/
+
+files=(
+  .config
+  .gitconfig
+  .gitignore_global
+  .vimrc
+  .zshrc
+  .zshrc.custom
+)
+path=`pwd`
+for f in "${files[@]}"; do
+  ln -svF $path/$f ~/
+done
+
+mkdir -p ~/.vim/autoload
+ln -svF $path/.vim/autoload/pathogen.vim ~/.vim/autoload/
+[[ -d ~/.vim/bundle/vundle ]] || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
+
+[[ -d ~/.oh-my-zsh ]] || git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+
 vim +BundleInstall +qall
 
-ln -s com.googlecode.iterm2.plist  $HOME/Library/Preferences/com.googlecode.iterm2.plist
+ln -sfv $path/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
 
-mkdir -p $HOME/.ssh
-cp .ssh/config $HOME/.ssh/
+mkdir -p ~/.ssh
+ln -sfv $path/.ssh/config ~/.ssh/
