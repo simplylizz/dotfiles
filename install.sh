0d9df95a16dd5770b34288ca106d1216d0ff05c7
Anton Yanchenko <simplylizz@gmail.com>
2020-12-19 10:44:14 +0100

    Add iterm2 config
    Unfortunately it's binary :(

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+set -eo pipefail
+
 # TODO: add backups
 # TODO: make links, not copy
 git submodule update --init --recursive
 cp -rf .vimrc .vim .zshrc* .oh-my-zsh .gitconfig .gitignore_global .config ~/
 vim +BundleInstall +qall
+
+ln -s com.googlecode.iterm2.plist  $HOME/Library/Preferences/com.googlecode.iterm2.plist
