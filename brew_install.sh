09874763fa992a00e1b70906639077ebfde9f16e
Anton Yanchenko <simplylizz@gmail.com>
2024-01-02 14:59:44 +0100

    Add fish

--- brew_install.sh
+++ brew_install.sh
@@ -19,6 +19,20 @@ brew install \
     telnet \
     neovim \
     watch \
+    fish \
 
 # otherwise ctrl+r won't work
 $(brew --prefix)/opt/fzf/install
+
+if [ grep 'homebrew' ~/.config/fish/config.fish ]; then
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' > ~/.config/fish/config.fish_new
+  cat  ~/.config/fish/config.fish >> ~/.config/fish/config.fish_new
+  mv ~/.config/fish/config.fish{_new,}
+fi
+
+if status is-interactive
+  jump shell fish | source
+end
+
+sudo echo $(which fish) >> /etc/shells
+chsh -s $(which fish)
