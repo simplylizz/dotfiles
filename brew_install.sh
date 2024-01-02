fcbe884dedcea21101e63d540d5549b42392b42f
Anton Yanchenko <anton.ianchenko@klm.com>
2024-01-02 15:46:40 +0100

    Fix aliases

--- brew_install.sh
+++ brew_install.sh
@@ -24,41 +24,37 @@ brew install \
 # otherwise ctrl+r won't work
 $(brew --prefix)/opt/fzf/install
 
-fconfig="~/.config/fish/config.fish"
+fconfig="$HOME/.config/fish/config.fish"
 
-if grep -q 'homebrew' "$fconfig"; then
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' > ~/.config/fish/config.fish_new
-  cat  ~/.config/fish/config.fish >> ~/.config/fish/config.fish_new
-  mv ~/.config/fish/config.fish{_new,}
-fi
-
-if ! grep -q 'ENV_TYPE' "$fconfig"; then
+if ! grep -q 'homebrew' "$fconfig"; then
   echo '
 if status is-interactive
   jump shell fish | source
 end
 
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 export PATH="$PATH:$HOME/go/bin:$HOME/.docker/bin"
 export ENV_TYPE=dev
-' >> "$fconfig"
+' > "$fconfig"
 fi
 
-fpath="~/.config/fish/functions"
+fpath="$HOME/.config/fish/functions"
 
-echo 'function g --wraps=git --description 'alias g=git'
-  git $argv;
-end' > "$fpath//g.fish"
+echo 'function g --wraps=git --description '\''alias g=git'\''
+  git $argv; 
+end' > "$fpath/g.fish"
 
-echo 'function k --wraps=kubectl --description 'alias k=kubectl'
-  kubectl $argv;
+echo 'function k --wraps=kubectl --description '\''alias k=kubectl'\''
+  kubectl $argv; 
 end' > "$fpath/k.fish"
 
-echo 'function ls --wraps=exa --description 'alias ls=exa'
-  exa $argv;
+echo 'function ls --wraps=exa --description '\''alias ls=exa'\''
+  exa $argv; 
 end' > "$fpath/ls.fish"
 
-echo 'function rc --wraps='rg --color always' --description 'alias rc=rg --color always'
-  rg --color always $argv;
+echo 'function rc --wraps=rg --description '\''alias rc=rg --color always'\''
+  rg --color always $argv; 
 end' > "$fpath/rc.fish"
 
 if ! grep -q fish /etc/shells; then
