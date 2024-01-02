238fe483023fdb8998578ada0d08d223286a8724
Anton Yanchenko <simplylizz@gmail.com>
2024-01-02 15:18:38 +0100

    Add fish shortcuts

--- brew_install.sh
+++ brew_install.sh
@@ -24,15 +24,42 @@ brew install \
 # otherwise ctrl+r won't work
 $(brew --prefix)/opt/fzf/install
 
-if [ grep 'homebrew' ~/.config/fish/config.fish ]; then
+fconfig="~/.config/fish/config.fish"
+
+if grep -q 'homebrew' "$fconfig"; then
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' > ~/.config/fish/config.fish_new
   cat  ~/.config/fish/config.fish >> ~/.config/fish/config.fish_new
   mv ~/.config/fish/config.fish{_new,}
 fi
 
+if ! grep -q 'ENV_TYPE' "$fconfig"; then
+  echo '
 if status is-interactive
   jump shell fish | source
 end
+
+export PATH="$PATH:$HOME/go/bin:$HOME/.docker/bin"
+export ENV_TYPE=dev
+' >> "$fconfig"
+fi
+
+fpath="~/.config/fish/functions"
+
+echo 'function g --wraps=git --description 'alias g=git'
+  git $argv;
+end' > "$fpath//g.fish"
+
+echo 'function k --wraps=kubectl --description 'alias k=kubectl'
+  kubectl $argv;
+end' > "$fpath/k.fish"
+
+echo 'function ls --wraps=exa --description 'alias ls=exa'
+  exa $argv;
+end' > "$fpath/ls.fish"
+
+echo 'function rc --wraps='rg --color always' --description 'alias rc=rg --color always'
+  rg --color always $argv;
+end' > "$fpath/rc.fish"
 
 sudo echo $(which fish) >> /etc/shells
 chsh -s $(which fish)
